11eba6b84f111992501c9f96a771c1cf24109b43
DavidHermi <dhermidaperez@danielcastelao.org>
2022-02-02 14:14:12 +0100

    Segundo cambio colaborador

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Primer Commit MASTER");
         System.out.println("Primer commit COLABORADOR");
+        System.out.println("Segundo cambio COLABORADOR");
     }
 
 }
